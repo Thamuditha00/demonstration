cffa44b3816cacb6261753e446b29341c08c730e
THAMUDITHA P V G S <2020CS183@stu.ucsc.cmb.ac.lk>
2022-12-07 19:12:22 +0530

    add css to manager registration form

--- manager/register.php
+++ manager/register.php
@@ -15,8 +15,50 @@ if(isset($_POST['register'])){
 
 ?>
 
-<form action="" method="post">
-    <input type="text" name="username" placeholder="Username">
-    <input type="password" name="password" placeholder="Password">
-    <input type="submit" name="register" value="Register">
-</form>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles.css">
+    <title>Register</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <div>
+            <h2> Register</h2>
+        </div>
+
+        <div class="loginDiv">
+            <form action="#" method="post">
+
+                <div>
+                    <input type="text" name="username" placeholder="Username" size="40">
+                </div>
+
+                <div>
+                    <input type="password" name="password" placeholder="Password" size="40">
+                </div>
+
+                <div>
+                    <button name="register" class="login-btn">Register</button>
+                </div>
+
+            </form>
+
+        </div>
+
+        <div>
+            <p>Already have an account? <a href="login.php">Login?</a></p>
+        </div>
+
+    </div>
+
+
+</body>
+
+</html>
