75d0d00c523ecdfb717dd5d489a220e7523ea021
pasangimhana <pasangimhanaofficial@gmail.com>
2022-12-15 10:45:52 +0530

    CSS Redesigned

--- cho/managerRegister.php
+++ cho/managerRegister.php
@@ -1,69 +1,90 @@
-<?php 
+<?php
 include 'config.php';
 error_reporting(0);
 
 if (isset($_POST['register'])) {
-	$id = uniqid('manager', true);
-	$username = $_POST['username'];
-	$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $id = uniqid('manager', true);
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-	$name = $_POST['name'];
-	$age = $_POST['age'];
-	$NIC = $_POST['NIC'];
-	$gender = $_POST['gender'];
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $NIC = $_POST['NIC'];
+    $gender = $_POST['gender'];
 
-	try {
-		$sql = "INSERT INTO manager (id, Name, age,NIC,gender) VALUES ('$id', '$name', '$age','$NIC','$gender')";
-		if($conn->query($sql) === TRUE) {
-			header("Location: index.php");
-		}
-	} catch(PDOException $e) {
-		echo "Error: " . $e->getMessage();
-	}
+    try {
+        $sql = "INSERT INTO manager (id, Name, age,NIC,gender) VALUES ('$id', '$name', '$age','$NIC','$gender')";
+        if ($conn->query($sql) === TRUE) {
+            header("Location: index.php");
+        }
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
 
 
-	try {
-		$sql = "INSERT INTO users (id, username, password) VALUES ('$id', '$username', '$password')";
-		if($conn->query($sql) === TRUE) {
-			header("Location: index.php");
-		}
-	} catch(PDOException $e) {
-		echo "Error: " . $e->getMessage();
-	}
+    try {
+        $sql = "INSERT INTO users (id, username, password) VALUES ('$id', '$username', '$password')";
+        if ($conn->query($sql) === TRUE) {
+            header("Location: index.php");
+        }
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
 }
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href="signup.css" rel="stylesheet" type="text/css">
-	<link href="logo.png" rel="icon">
-	<title>Sign up</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="logo.png" rel="icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/boxicons/2.0.7/css/boxicons.min.css"/>
+    <link rel="stylesheet" href="sidebar.css">
+    <link rel="stylesheet" href="form.css">
+    <title>Sign up</title>
 </head>
 <body>
-		<form action="" method="POST" class="form" id="registerManager" onsubmit="isValidate()">
-        <p class="login-text" style="font-size: 2rem; font-weight: 800;">Please add manager's details</p>
-				<input type="text" placeholder="Name" name="name"required>
-				<input type="text" placeholder="age" name="number"required>
-				<input type="text" placeholder="NIC" name="NIC" onkeyup="validateNIC()" required>
-				<span id="NICError"></span>
+<div class="container">
+    <?php include 'sidebar.php'; ?>
+    <div class="main">
+        <div class="form">
+            <h1>Add Manager details</h1>
+            <form action="" method="POST" onsubmit="isValidate()" id="manager-reg">
+                <input type="text" placeholder="Name" name="name" required>
+                <input type="text" placeholder="age" name="number" required>
+                <input type="text" placeholder="NIC" name="NIC" onkeyup="validateNIC()" required>
+                <span id="NICError"></span>
 
-				<select name=gender>
-					<option value=""> Select </option>
-					<option value="male"> Male </option>
-					<option value="female"> Female </option>
-				</select>
+                <select name=gender>
+                    <option value=""> Select</option>
+                    <option value="male"> Male</option>
+                    <option value="female"> Female</option>
+                </select>
 
-				<input type="text" placeholder="Username" name="username" onkeyup="validateUsername()" required>
-				<span id="usernameError"></span>
-				<input type="password" placeholder="Password" name="password" onkeyup="validatePassword()" required>
-				<span id="passwordError"></span>
-				<button name="register" class="button">Register</button>
-		</form>
-	</div>
+                <input type="text" placeholder="Username" name="username" onkeyup="validateUsername()" required>
+                <span id="usernameError"></span>
+
+                <input type="password" placeholder="Password" name="password" onkeyup="validatePassword()" required>
+                <span id="passwordError"></span>
+                <div class="pw-list">
+                    <ul class="pw-list">
+                        <li>Password length should be between 8 and 15 characters</li>
+                        <li>Password should contain at least one uppercase letter</li>
+                        <li>Password should contain at least one lowercase letter</li>
+                        <li>Password should contain at least one number</li>
+                        <li>Password should contain at least one special character</li>
+                    </ul>
+                </div>
+                <div class="submit-button">
+                <button name="register" class="button">Register</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 </body>
 
-	<script src="./managerRegister.js"></script>
+<script src="./managerRegister.js"></script>
 </html>
--- cho/regcc.php
+++ cho/regcc.php
@@ -23,48 +23,52 @@ if (isset($_POST['submit'])) {
     }
 
 
-
 }
 ?>
 
 
-html>
+<html>
 <head>
     <title> Register a Community Center</title>
-    <link rel="stylesheet" href="regstyle.css">
+    <link rel="stylesheet" href="style.css">
     <link rel="icon" href="logo.png" class="logo">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/boxicons/2.0.7/css/boxicons.min.css"/>
+    <link rel="stylesheet" href="sidebar.css">
+    <link rel="stylesheet" href="form.css">
 </head>
 
 <body>
 <div class="container">
+    <?php include 'sidebar.php'; ?>
 
-    <div class="sidenav">
-        <img class="logo" src="logo.png">
-        <a href="#"> Users </a>
-        <a href="index.php" class="active"> Community Center </a>
-        <a href="#">Complaints</a>
-        <a href="logout.php"> Logout </a>
-    </div>
-    <div class="main-area">
-        <h2>Add a Community Center</h2>
-        <form method="POST" action="" class="formStyle" id="registerCC" onsubmit="return isValidated()">
+    <div class="main">
+        <div class="form">
+            <h2>Register a Community Center</h2>
+            <form method="POST" action="" class="formStyle" id="registerCC" onsubmit="return isValidated()">
 
-            <p>Address</p>
-            <input type="text" placeholder="Address" name="address" required/>
-            <p>Area</p>
-            <input type="text" placeholder="Area" name="area" onchange="validateAreas()"/>
-            <span id="areaError"></span>
-            <p> Contact Number</p>
-            <input type="text" placeholder="Contact Number" name="contactNumber" onkeyup="validateContact()" required/>
-            <span id="contactError"></span>
-            <p> Email Address</p>
-            <input type="email" placeholder="Email Address" name="email" onkeyup="validateEmail()" required/>
-            <span id="emailError"></span>
-            <p> Fax</p>
-            <input type="text" placeholder="Fax" name="fax" onkeyup="validateFax()" required/>
-            <span id="faxError"></span>
-            <button name="submit" value="done" class="btn"> Submit</button>
-        </form>
+                <label for="address">Address</label>
+                <input type="text" placeholder="Address" name="address" required/>
+                <label for="area">Area</label>
+                <input type="text" placeholder="Area" name="area" onchange="validateAreas()"/>
+                <span id="areaError"></span>
+                <label for="contactNumber">Contact Number</label>
+                <input type="text" placeholder="Contact Number" name="contactNumber" onkeyup="validateContact()"
+                       required/>
+                <span id="contactError"></span>
+                <label for="email">Email</label>
+                <input type="email" placeholder="Email Address" name="email" onkeyup="validateEmail()" required/>
+                <span id="emailError"></span>
+                <label for="fax">Fax Number</label>
+                <input type="text" placeholder="Fax" name="fax" onkeyup="validateFax()" required/>
+                <span id="faxError"></span>
+
+                <div class="submit-button">
+
+                <button name="submit" value="done" class="btn">Continue</button>
+                    <p>Add Manager Details Next<i class='bx bx-right-arrow-alt' ></i></p>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
 
